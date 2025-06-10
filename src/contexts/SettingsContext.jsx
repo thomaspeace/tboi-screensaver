ad19a4010b7e3fedeeae04994864895b124e5efa
thomaspeace <thomasjpeace29@gmail.com>
2025-06-10 21:07:38 +0100

    feat(settings context): add provider and initial values

--- src/contexts/SettingsContext.jsx
+++ src/contexts/SettingsContext.jsx
@@ -2,6 +2,28 @@ import { createContext, useContext, useState } from "react";
 
 const SettingsContext = createContext();
 
-const [settings, setSettings] = useState({
-  //
-});
+function SettingsProvider({ children }) {
+  const [settings, setSettings] = useState({
+    app: {
+      backgroundColor: "rgb(74, 74, 74)",
+      // more settings for app
+    },
+    clock: {
+      showStreak: true,
+    },
+    time: {
+      timeFormat: "HH:mm:ss",
+    },
+    date: {
+      dateFormat: "do MMMM yyyy",
+    },
+  });
+
+  const value = {};
+
+  return (
+    <SettingsContext.Provider value={value}>
+      {children}
+    </SettingsContext.Provider>
+  );
+}
