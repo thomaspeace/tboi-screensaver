7ac894960cc6ac285d2d8bb1baa903fb140cbf66
thomaspeace <thomasjpeace29@gmail.com>
2025-06-10 15:54:05 +0100

    style(streak): edit the SVG to change the colour from black HEX(#1a0101)

--- src/components/Clock/TimeDisplay.jsx
+++ src/components/Clock/TimeDisplay.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { format } from "date-fns";
 import "./Clock.css";
-import streak_effect from "../../assets/svgs/streak_effect.svg";
 
 export default function TimeDisplay() {
   const [time, setTime] = useState(new Date());
