dd319cfbd8c29a64e19a4f1d4e09de7022fdf368
thomaspeace <thomasjpeace29@gmail.com>
2025-06-10 21:56:06 +0100

    feat(app): use onMouseLeave and onMouseEnter to detect when the mouse is on the screen to hide and show the header buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import Clock from "./components/Clock/Clock";
 import { useSettings } from "./contexts/SettingsContext";
 import { useWakeLock } from "./hooks/useWakeLock";
 import { useFullscreen } from "./hooks/useFullscreen";
+import { useState } from "react";
 
 function App() {
   useWakeLock();
   const { settings } = useSettings();
   const { isFullscreen, toggleFullscreen } = useFullscreen();
+  const [isHovering, setIsHovering] = useState(false);
 
   return (
     <div
       className="app-container"
       style={{ backgroundColor: settings.app.backgroundColor }}
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
     >
-      <div className="app-header">
+      <div className={`app-header ${!isHovering ? "hidden" : ""}`}>
         <button className="header-button" onClick={toggleFullscreen}>
           {isFullscreen ? "Exit Fullscreen" : "Fullscreen"}
         </button>
