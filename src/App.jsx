62f23940d3a0076a9ced27ae5a535301219c5754
thomaspeace <thomasjpeace29@gmail.com>
2025-06-11 18:26:11 +0100

    feat(app): hide header buttons after 3 seconds of mouse inactivity
    before it would only be hidden if the mouse wasn't on the page.
    however this wont work for single monitor setups if you just want it displayed on the single screen.
    now, a useEffect is added and when the mouse moves, using an eventListener we call the handleMouseMove fn, which resets the timer of inactivity.

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,45 @@ import Clock from "./components/Clock/Clock";
 import { useSettings } from "./contexts/SettingsContext";
 import { useWakeLock } from "./hooks/useWakeLock";
 import { useFullscreen } from "./hooks/useFullscreen";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SettingsModal from "./components/SettingsModal/SettingsModal";
 
 function App() {
   useWakeLock();
   const { settings } = useSettings();
   const { isFullscreen, toggleFullscreen } = useFullscreen();
-  const [isHovering, setIsHovering] = useState(false);
+  const [isMouseMoving, setIsMouseMoving] = useState(false);
   const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+
+  useEffect(() => {
+    let inactivityTimer;
+
+    const handleMouseMove = () => {
+      setIsMouseMoving(true);
+      clearTimeout(inactivityTimer);
+      inactivityTimer = setTimeout(() => {
+        setIsMouseMoving(false);
+      }, 3000); // 3 seconds of mouse inactivity
+    };
+
+    // initial setup
+    handleMouseMove();
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    // clean up fn
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      clearTimeout(inactivityTimer);
+    };
+  }, []);
 
   return (
     <div
       className="app-container"
       style={{ backgroundColor: settings.app.backgroundColor }}
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
     >
-      <div className={`app-header ${!isHovering ? "hidden" : ""}`}>
+      <div className={`app-header ${!isMouseMoving ? "hidden" : ""}`}>
         <button className="header-button" onClick={toggleFullscreen}>
           {isFullscreen ? "Exit Fullscreen" : "Fullscreen"}
         </button>
